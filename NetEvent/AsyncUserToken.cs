4d35ec84710bebe46bbab3241960b25b1c4e8bae
guoyichao <guoyichao@gmail.com>
2014-02-07 10:39:37 +0800

    ok

---
+++ NetEvent/AsyncUserToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TinyNetEvent
+{
+    class AsyncUserToken
+    {
+        public Socket Socket { get; set; }
+    }
+}
---
+++ NetEvent/SocketAsyncEventArgsPool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TinyNetEvent
+{
+    class SocketAsyncEventArgsPool : Stack<SocketAsyncEventArgs>
+    {
+        public SocketAsyncEventArgsPool(int max)
+        {
+
+        }
+    }
+}
